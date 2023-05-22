dc521a2c026376a48aa606dc81150a107c3e0bac
Inom T <i@inomoz.ru>
2023-05-22 16:09:35 +0600

    Optimize win setup notes

--- ventoy.md
+++ ventoy.md
@@ -52,3 +52,68 @@ canonicalUrl: https://www.ventoy.net/en/index.html
       -L          Label of the 1st exfat partition (default is Ventoy)
       -n          try non-destructive installation (only for install)
    ```
+
+
+## How to copy ISO file to Ventoy USB drive and validate it
+
+### Windows
+
+Checksums are verified, check this section [[win_setup_script]]
+```sh
+export VENTOY_PATH="/media/Ventoy"
+export WIN_ISO_CHECKSUM="c90a6df8997bf49e56b9673982f3e80745058723a707aef8f22998ae6479597d"
+export ISO_FILE="en-us_windows_10_enterprise_ltsc_2021_x64_dvd_d289cf96.iso"
+
+# Mount USB drive
+udiskie-mount -a
+
+# Check ventoy USB drive is mounted
+[ -d $VENTOY_PATH ] || exit 1
+
+# Update ventoy path
+VENTOY_PATH="$VENTOY_PATH/_win"
+
+# Create _win directory
+mkdir -p "$VENTOY_PATH"
+
+cp "$ISO_FILE" "$VENTOY_PATH"
+
+# Create SHA-256 file and validate it
+echo "$WIN_ISO_CHECKSUM  $ISO_FILE" > "$VENTOY_PATH/$ISO_FILE.sha256.txt"
+sha256sum -c "$VENTOY_PATH/$ISO_FILE.sha256.txt"
+```
+
+### Arch Linux
+
+To download, I use this URL: <https://geo.mirror.pkgbuild.com/iso/latest/>
+
+```sh
+## Step 1 prepare
+
+export ISO_FILE="archlinux-x86_64.iso"
+export SIGNER_FILE="$ISO_FILE.sig"
+export ARCH_ISO_URL="https://geo.mirror.pkgbuild.com/iso/latest/$ISO_FILE"
+export ARCH_SIG_URL="https://geo.mirror.pkgbuild.com/iso/latest/$SIGNER_FILE"
+export VENTOY_PATH="/media/Ventoy"
+
+## Verify requiremets installed
+sudo pacman -S --needed aria2 gnupg udiskie
+
+# Mount USB drive
+udiskie-mount -a
+
+# Check ventoy USB drive is mounted
+[ -d $VENTOY_PATH ] || exit 1
+
+# Update ventoy path
+VENTOY_PATH="$VENTOY_PATH/_linux"
+
+# Create _linux directory
+mkdir -p "$VENTOY_PATH"
+
+## Step 2 download and verify
+cd "$VENTOY_PATH"
+aria2c --max-connection-per-server 8 "$ARCH_ISO_URL"
+aria2c "$ARCH_SIG_URL"
+gpg --keyserver-options auto-key-retrieve --verify "$SIGNER_FILE"
+```
--- win/win-setup.ps1
+++ win/win-setup.ps1
@@ -1,121 +1,18 @@
-# Install WinGet
-# Based on this gist: https://gist.github.com/crutkas/6c2096eae387e544bd05cde246f23901
-# To run gist from powershell:
-# iex ((New-Object System.Net.WebClient).DownloadString('https://gist.githubusercontent.com/Inom-Turdikulov/6c0b3b2e40f54fe8ad5cee8d1dd83317/raw'))
-#
-# ISO file
-#
-# FILE: en-us_windows_10_enterprise_ltsc_2021_x64_dvd_d289cf96.iso
-# MD5: b5a7be560dbd73619945129e52be1b5f
-# SHA-1: 2fb2897373c4f71b06f4490943b3d564b0f0fd6d
-# SHA-256: c90a6df8997bf49e56b9673982f3e80745058723a707aef8f22998ae6479597d
-# https://msdn.rg-adguard.net/public.php?seach=us_windows_10_enterprise_ltsc_2021_x64_dvd_d289cf96&str=25#60680
-#
-# sha256sum en-us_windows_10_enterprise_ltsc_2021_x64_dvd_d289cf96.iso | grep c90a6df8997bf49e56b9673982f3e80745058723a707aef8f22998ae6479597d
-#
-# LTSC release require additioanl steps to install winget
-#
-# VC++ v14 Desktop Framework Package
-#
-# https://docs.microsoft.com/en-gb/troubleshoot/cpp/c-runtime-packages-desktop-bridge#how-to-install-and-update-desktop-framework-packages
-#
-# Download the correct VC++ v14 Desktop Framework Package for your architecture.
-# Install with: Add-AppxPackage -Path "PATH TO FILE"
-#
-# Installing Winget
-# Make sure all dependencies are installed before doing this!
-# From the latest release download the .msixbundle install- and .xml license file: https://github.com/microsoft/winget-cli/releases
-# Install with: Add-AppxProvisionedPackage -Online -PackagePath "PATH TO MSIXBUNDLE" -LicensePath "PATH TO XML" -Verbose
-# Wait for the install to finish and you're done. You may need to restart the terminal, or reboot your pc.
-# Verify that the installation succeeded by running winget in PowerShell. If no errors occur then you're done!
+# Install Scoop
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
+irm get.scoop.sh | iex
 
-$hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
-if (!$hasPackageManager -or [version]$hasPackageManager.Version -lt [version]"1.10.0.0") {
-    "Installing winget Dependencies"
-    Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
-
-    $releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
-
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $releases = Invoke-RestMethod -uri $releases_url
-    $latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith('msixbundle') } | Select -First 1
-
-    "Installing winget from $($latestRelease.browser_download_url)"
-    Add-AppxPackage -Path $latestRelease.browser_download_url
-}
-else {
-    "winget already installed"
-}
-
-#Configure WinGet
-Write-Output "Configuring winget"
-
-#winget config path from: https://github.com/microsoft/winget-cli/blob/master/doc/Settings.md#file-location
-$settingsPath = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json";
-$settingsJson =
-@"
-    {
-        // For documentation on these settings, see: https://aka.ms/winget-settings
-        "experimentalFeatures": {
-          "experimentalMSStore": true,
-        }
-    }
-"@;
-$settingsJson | Out-File $settingsPath -Encoding utf8
-
-#Install New apps
-#    @{name = "Microsoft.WindowsTerminal"; source = "msstore" },
-Write-Output "Installing Apps"
-$apps = @(
-    @{name = "Git.Git" },
-    @{name = "Neovim.Neovim" },
-    @{name = "Valve.Steam" },
-    @{name = "Microsoft.PowerShell" },
-    @{name = "Microsoft.WindowsTerminal" },
-    @{name = "SlackTechnologies.Slack" },
-    @{name = "BraveSoftware.BraveBrowser" },
-    @{name = "7zip.7zip" }
-);
-Foreach ($app in $apps) {
-    $listApp = winget list --exact -q $app.name --accept-source-agreements
-    if (![String]::Join("", $listApp).Contains($app.name)) {
-        Write-host "Installing:" $app.name
-        if ($app.source -ne $null) {
-            winget install --exact --silent $app.name --source $app.source --accept-package-agreements --accept-source-agreements
-        }
-        else {
-            winget install --exact --silent $app.name --accept-package-agreements --accept-source-agreements
-        }
-    }
-    else {
-        Write-host "Skipping Install of " $app.name
-    }
-}
-
-#Remove Apps
-Write-Output "Removing Apps"
-
-$apps = "*3DPrint*",
-        "Microsoft.MixedReality.Portal",
-        "Microsoft.Bing*",
-        "Microsoft.GamingApp",
-        "Microsoft.WindowsFeedbackHub",
-        "Microsoft.549981C3F5F10",
-        "Microsoft.MicrosoftStickyNotes",
-        "Microsoft.MicrosoftSolitaireCollection",
-        "Microsoft.WindowsMaps",
-        "Microsoft.WindowsSoundRecorder",
-        "Microsoft.YourPhone",
-        "Microsoft.ZuneMusic",
-        "Microsoft.ZuneVideo",
-        "microsoft.windowscommunicationsapps",
-        "Microsoft.Windows.Photos"
-
-Foreach ($app in $apps)
-{
-    Write-host "Uninstalling:" $app
-    Get-AppxPackage -allusers $app  | Remove-AppxPackage
-}
+# Install packages
+scoop install main/git
+scoop bucket add main
+scoop bucket add extras
+scoop install main/neovim
+scoop install extras/windows-terminal
+scoop install extras/slack
+scoop install extras/brave
+scoop install extras/vcredist2015
+scoop install extras/windirstat
+scoop install extras/cru
 
 # Create directories
 New-Item -ItemType Directory -Path "C:\bin"
@@ -130,5 +27,33 @@ Invoke-WebRequest $url -OutFile $outputPath
 echo "Installing WSL"
 echo "wsl --install"
 
+echo "---"
 echo "Enable dark mode"
 echo "New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0"
+echo "Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0"
+
+echo "---"
+echo "TO disable antivirus, run the following commands in PowerShell:"
+echo "Set-MpPreference -DisableRealtimeMonitoring $true"
+echo "Set-MpPreference -SubmitSamplesConsent NeverSend"
+echo "Set-MpPreference -MAPSReporting Disable"
+
+echo "Fully Disable defneder. Don't use it on important machines, it can be gone forever."
+echo "https://github.com/jbara2002/windows-defender-remover/releases"
+
+echo "Also need disable anti-virus notifications in the Action Center, right-click in tray on defender icon, view notifications options"
+
+echo "---"
+echo "To remove crapware, disable autostart items in Windows tasks manager, and uninstall apps from the start menu"
+
+echo "---"
+echo "Possible useful commands: Install-Module -Name DisplaySettings;Set-DisplayResolution -Width 1920 -Height 1024"
+
+echo "---"
+echo "Disable wallpaper"
+echo "Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value '' rundll32.exe user32.dll, UpdatePerUserSystemParameters"
+
+echo "---"
+echo "Make taskbar icons small"
+echo "Set-ItemProperty -path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\' -name TaskbarSmallIcons -value 1"
+echo "Stop-Process -name explorer  # explorer.exe restarts automatically after stopping"
