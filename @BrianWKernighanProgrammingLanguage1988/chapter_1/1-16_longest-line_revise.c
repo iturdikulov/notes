0db74a69719fbdb3d60287df72ab31af9092e8da
Inom T <i@inomoz.ru>
2023-06-01 00:05:37 +0600

    Fix formatting / conflict getline name

--- @BrianWKernighanProgrammingLanguage1988/chapter_1/1-16_longest-line_revise.c
+++ @BrianWKernighanProgrammingLanguage1988/chapter_1/1-16_longest-line_revise.c
@@ -5,40 +5,40 @@ of the text.
 */
 
 #include <stdio.h>
-#define MAXLINE 1000    /* maximum input line size */
+#define MAXLINE 1000 /* maximum input line size */
 
-int getline(char line[], int maxline);
+int custom_getline(char line[], int maxline);
 void copy(char to[], char from[]);
 
 /* print longest input line */
 int main()
 {
-    int len;            /* current line length */
-    int max;            /* maximum length seen so far */
-    char line[MAXLINE];     /* current input line */
-    char longest[MAXLINE];  /* longest line saved here */
+    int len; /* current line length */
+    int max; /* maximum length seen so far */
+    char line[MAXLINE]; /* current input line */
+    char longest[MAXLINE]; /* longest line saved here */
 
     max = 0;
-    while ((len = getline(line, MAXLINE)) > 0) {
+    while ((len = custom_getline(line, MAXLINE)) > 0) {
         printf("len: %d\n", len);
         if (len > max) {
             max = len;
             copy(longest, line);
         }
     }
-    if (max > 0)    /* there was a line */
+    if (max > 0) /* there was a line */
         printf("%s", longest);
     return 0;
 }
 
 /* getline: read a line into s, return length */
-int getline(char s[], int lim)
+int custom_getline(char s[], int lim)
 {
     int c, i, j;
     j = 0;
     for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i) {
         if (i < lim - 2) {
-            s[j] = c;   /* line still in boundaries */
+            s[j] = c; /* line still in boundaries */
             ++j;
         }
     }
