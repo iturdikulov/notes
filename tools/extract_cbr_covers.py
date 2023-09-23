f7b06a4268d849e9bbd8af38209bb87496574f44
Inom Turdikulov <i@inomoz.ru>
2023-09-24 03:25:03 +0600

    Add new script to extract covers from cbr files

---
+++ tools/extract_cbr_covers.py
@@ -0,0 +1,16 @@
+# You need install rarfile package
+# nix-shell -p python310Packages.rarfile
+
+import rarfile
+from pathlib import Path
+
+directory = ""  # Replace with your desired directory path
+cbr_path = Path.home() / "Arts_and_Entertainment/literature/Katsuhiro Otomo"
+
+if __name__ == "__main__":
+    for file in cbr_path.glob('**/*.cbr'):
+        with rarfile.RarFile(file) as rf:
+            first_file = rf.namelist()[0]  # Get the name of the first file in the archive
+            print("Extracting: " + first_file)
+            rf.extract(first_file, directory)  # Extract the first file to the same directory
+
