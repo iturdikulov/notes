19a564f8c06639cd537012fc30bc2c36a092fac8
Inom Turdikulov <i@inomoz.ru>
2023-09-25 01:01:12 +0600

    Add shebang / change default dir

--- tools/extract_cbr_covers.py
+++ tools/extract_cbr_covers.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # You need install rarfile package
 # nix-shell -p python310Packages.rarfile
 
@@ -5,7 +6,7 @@ import rarfile
 from pathlib import Path
 
 directory = ""  # Replace with your desired directory path
-cbr_path = Path.home() / "Arts_and_Entertainment/literature/Katsuhiro Otomo"
+cbr_path = Path.home() / "Arts_and_Entertainment/literature/Yukito Kishiro"
 
 if __name__ == "__main__":
     for file in cbr_path.glob('**/*.cbr'):
