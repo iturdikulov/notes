7913c95e9cd26a0480e73256c4424389f0980de0
Inom Turdikulov <i@inomoz.ru>
2024-03-06 23:45:14 +0600

    Update keyboard keymap

--- external/keymap.c
+++ external/keymap.c
@@ -55,8 +55,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,      KC_F9,   KC_F10,  KC_F11,       KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,       KC_9,    KC_0,    KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_LT,   KC_LCBR, KC_LBRC, KC_LPRN, KC_EQL,  KC_RGUI, KC_ESC,  LSFT(KC_P), KC_P,    KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_GT,   KC_RCBR, KC_RBRC, KC_RPRN, KC_DLR,  KC_EXLM, KC_DQUO,    KC_QUOT, KC_MINS, LSFT(KC_EQL), KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_LT,   KC_LBRC, KC_LCBR, KC_LPRN, KC_EQL,  KC_RGUI, KC_ESC,  LSFT(KC_P), KC_P,    KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_GT,  KC_RBRC,  KC_RCBR, KC_RPRN, KC_DLR,  KC_EXLM, KC_DQUO,    KC_QUOT, KC_MINS, LSFT(KC_EQL), KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, TG(1)),
     /*
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
