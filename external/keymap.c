70f1a297e3c6d238be087fa230d55d2d9c720b64
Inom Turdikulov <i@inomoz.ru>
2024-11-06 23:11:27 +0300

    Update my QMK keymap

--- external/keymap.c
+++ external/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,   KC_Y,  KC_U,  KC_I,  KC_O,   KC_P,     KC_LBRC, KC_RBRC, KC_BSLS /******/, KC_VOLU,
     KC_LCTL,   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,   KC_H,  KC_J,  KC_K,  KC_L,   KC_SCLN,  KC_QUOT,  KC_ENT /**************/, KC_VOLD,
     LSF, LSF,   KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,   KC_N,  KC_M,  KCOM,  KC_DOT, KC_SLSH,         KC_RSFT, KC_RSFT /******/, KC_MPLY,
-    LA_QWE, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC /***************/, LA_SYM, KC_RALT, KC_RCTL,   KC_MPRV, KC_MNXT         , KC_MSTP
+    LA_QWE, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC /***************/, LA_SYM, KC_RALT, KC_RCTL,   KC_MPRV, KC_MNXT         , ORGUI
   ),
 
   [SYM] = LAYOUT(
